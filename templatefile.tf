becbef5cf168086425cf74c2e4e2c34377589f19
Sven Erik Knop <seknop@gmail.com>
2026-02-13 11:37:18 +0000

    Turn nodeclass.yaml into a template to be automatically filled.

---
+++ templatefile.tf
@@ -0,0 +1,17 @@
+variable "nodeclass_templatefile" {
+  default = "nodeclass.tftpl"
+}
+
+variable "inventory_file" {
+  default = "nodeclass.yaml"
+}
+
+resource "local_file" "nodeclass" {
+  content = templatefile("${path.module}/${var.nodeclass_templatefile}",
+    {
+      cluster-name = module.eks.cluster_name
+    }
+  )
+  filename = var.inventory_file
+  file_permission = "664"
+}
